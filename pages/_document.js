2277abcbf86dc50760cdb55c68cebf9201b306e3
Ngoc NT <tanngoc93@gmail.com>
2021-06-26 12:38:22 +0700

    Update file _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -21,14 +21,6 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0" />
 
-          <script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KBWXL2Z');`
-          }}></script>
-
           <link
             rel="icon"
             type="image/x-icon"
@@ -40,21 +32,8 @@ class MyDocument extends Document {
             href={`${config.assetPrefix}/static/css/styles.css`} />
         </Head>
         <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N3G3555" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-            }}
-          ></noscript>
           <Main />
           <NextScript />
-          <div id="fb-root"></div>
-          <div className="fb-customerchat"
-            attribution="setup_tool"
-            page_id="1619179571426959"
-            theme_color="#ff7e29"
-            logged_in_greeting="Hi! How can we help you?"
-            logged_out_greeting="Hi! How can we help you?">
-          </div>
           <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
         </body>
       </Html>
